54a583ef4b8e168d32ae6bec79d5a11e2c263856
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-02-10 10:28:41 +0200

    Added logging system and main method

---
+++ PlatformerProject/src/Log.h
@@ -0,0 +1,37 @@
+// Reused from OnyxEngine @ https://github.com/t-CK/OnyxEngine
+
+#pragma once
+
+// Vendors
+#include <spdlog/spdlog.h>
+
+
+
+class Log
+{
+public:
+	static void Init();
+
+	static std::shared_ptr<spdlog::logger>& GetLogger() { return s_Logger; }
+private:
+	static std::shared_ptr<spdlog::logger> s_Logger;
+};
+
+
+// Makros are left as ONYX_:: to indicate, that they have been reused
+#if _DEBUG
+							   		   
+#define ONYX_FATAL(...)        Log::GetLogger()->critical(__VA_ARGS__)
+#define ONYX_ERROR(...)        Log::GetLogger()->error(__VA_ARGS__)
+#define ONYX_WARNING(...)      Log::GetLogger()->warn(__VA_ARGS__)
+#define ONYX_INFO(...)         Log::GetLogger()->info(__VA_ARGS__)
+#define ONYX_TRACE(...)        Log::GetLogger()->trace(__VA_ARGS__)
+
+#else
+
+#define ONYX_FATAL(...)
+#define ONYX_ERROR(...)
+#define ONYX_WARNING(...)
+#define ONYX_INFO(...)
+
+#endif
---
+++ PlatformerProject/src/main.cpp
@@ -0,0 +1,8 @@
+#include "Log.h"
+
+int main()
+{
+	Log::Init();
+
+	return 0;
+}
