656cf657aa134ddceb639a4921f1242e3a7d2242
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-09-11 13:45:19 +0300

    Added VertexBufferLayout class and VertexBufferData struct

--- PlatformerProject/src/Platform/OpenGL/VertexBufferLayout.h
+++ PlatformerProject/src/Platform/OpenGL/VertexBufferLayout.h
@@ -22,35 +22,39 @@ namespace Renderer
 		}
 	};
 
-	template <typename T>
+	
 	class VertexBufferLayout
 	{
 	public:
 		VertexBufferLayout() :
-			m_Stride = 0 { }
+			m_Stride(0) { }
+
+		template <typename T>
+		void Push(unsigned int count) { }
+
 		template<>
 		void Push<float>(unsigned int count)
 		{
 			m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
-			m_Stride += VertexBufferElement::GetStrideOfType(GL_FLOAT) * count;
+			m_Stride += VertexBufferElement::GetSizeOfType(GL_FLOAT) * count;
 		}
-
 
 		template<>
 		void Push<unsigned int>(unsigned int count)
 		{
-			m_Elements.push_back(GL_UNSIGNED_INT, count, GL_FALSE);
+			m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
 			m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT) * count;
 		}
 
 		template<>
 		void Push<unsigned char>(unsigned int count)
 		{
-			m_Elements.push_back(GL_UNSIGNED_BYTE, count, GL_TRUE);
+			m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
 			m_Stride += VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE) * count;
 		}
 
 		inline unsigned int GetStride() { return m_Stride; }
+		inline const std::vector<VertexBufferElement>GetElements() const { return m_Elements; }
 
 	private:
 		std::vector<VertexBufferElement> m_Elements;
