7fb28e60edae6571c3efbaca51ee2c78baf08dc6
Aaron Tukiainen <81102463+t-CK@users.noreply.github.com>
2023-09-11 13:43:49 +0300

    Added VertexBufferLayout into vao and added necessary code to use it in VAP::AddBuffer

--- PlatformerProject/src/Platform/OpenGL/VAO.cpp
+++ PlatformerProject/src/Platform/OpenGL/VAO.cpp
@@ -1,4 +1,6 @@
 #include "VAO.h"
+#include "VBO.h"
+#include "VertexBufferLayout.h"
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -9,6 +11,23 @@ namespace Renderer
 	{
 		glGenVertexArrays(1, &m_RendererID);
 		Bind();
+	}
+
+	void VAO::AddBuffer(VBO& vbo, VertexBufferLayout& layout)
+	{
+		Bind();
+		vbo.Bind();
+
+		const auto& elements = layout.GetElements();
+
+		unsigned int offset = 0;
+		for (unsigned int i = 0; i < elements.size(); i++)
+		{
+			const auto& element = elements[i];
+			glEnableVertexAttribArray(i);
+			glVertexAttribPointer(i, elements[i].GetSizeOfType(elements[i].type), elements[i].type, elements[i].normalized, layout.GetStride(), (const void*)offset);
+			offset += element.count * VertexBufferElement::GetSizeOfType(element.type);
+		}
 	}
 
 	void VAO::Bind()
--- PlatformerProject/src/Platform/OpenGL/VAO.h
+++ PlatformerProject/src/Platform/OpenGL/VAO.h
@@ -2,10 +2,14 @@
 
 namespace Renderer
 {
+	class VBO;
+	class VertexBufferLayout;
 	class VAO
 	{
 	public:
 		VAO();
+
+		void AddBuffer(VBO& vbo, VertexBufferLayout& layout);
 
 		void Bind();
 		void UnBind();
